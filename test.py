e95af8d7cc26aca2edadf43330133ef9ce0975a9
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2021-11-28 12:58:02 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)#sets up the pin locations
 GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.IN)
 GPIO.setup(13, GPIO.IN)
 GPIO.setup(15, GPIO.IN)
 while True:
-	GPIO.output(7, GPIO.HIGH)
-	if(GPIO.input(11)==1):
+	GPIO.output(7, GPIO.HIGH)#turns on LED
+	if(GPIO.input(11)==1):#prints to screen based on which button is pushed
 		print("Button on GPIO 11")
 		time.sleep(1)
 	if(GPIO.input(13)==1):
